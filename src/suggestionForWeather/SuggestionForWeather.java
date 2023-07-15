5a2d301276b09021c1cab8efa2b6a68d17cf4c71
h.mertozgen@gmail.com <h.mertozgen@gmail.com>
2023-07-15 11:33:54 +0300

    SuggestionForWeather

---
+++ src/suggestionForWeather/SuggestionForWeather.java
@@ -0,0 +1,22 @@
+package suggestionForWeather;
+
+import java.util.Scanner;
+
+public class SuggestionForWeather {
+    public static void main(String[] args) {
+        int heat;
+        Scanner inp =new Scanner(System.in);
+        System.out.println("Sıcaklık giriniz: ");
+        heat = inp.nextInt();
+        
+        if (heat<5){
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if (5<heat && heat<15) {
+            System.out.println("Sinemaya gidebilirsiniz");
+        } else if (15<heat && heat<25) {
+            System.out.println("Pikniğe gidebilirsiniz");
+        }else {
+            System.out.println("Yüzmeye gidebilirsiniz");
+        }
+    }
+}
