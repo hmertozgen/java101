bf75fa3ed7e4fe26c305f9fbf8d542df5c0ca575
h.mertozgen@gmail.com <h.mertozgen@gmail.com>
2023-07-15 12:09:43 +0300

    AirplaneTicketPriceCalculator

--- src/airplaneTicketPriceCalculator/AirplaneTicketPriceCalculator.java
+++ src/airplaneTicketPriceCalculator/AirplaneTicketPriceCalculator.java
@@ -4,56 +4,41 @@ import java.util.Scanner;
 
 public class AirplaneTicketPriceCalculator {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-
-        double km, costPerKm = 0.1 , total;
-        byte type;
-        int age;
-        boolean isError = false;
-
-        System.out.print("Mesafeyi dahil ediniz : ");
-        km = inp.nextDouble();
-
-        System.out.print("Yaşınızı giriniz : ");
-        age = inp.nextInt();
-
+        // kullanicidan degiskenlerin degerini almak icin input nesnemizi kullanacagiz
+        Scanner input = new Scanner(System.in);
+        // degiskenlerimizi tanimliyoruz
+        int distance,age,tripType;
+        double total;
+        // kullanicidan degiskenlere deger girmelerini istiyoruz
+        System.out.print("Mesafeyi km türünden giriniz: ");
+        distance = input.nextInt();
+        System.out.print("Yaşınızı giriniz: ");
+        age = input.nextInt();
         System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
-        type = inp.nextByte();
-        total = km * costPerKm;
-
-        if (age<12){
-            total *= 0.5;
-        }
-        else if (age>=12 && age<=24) {
-            total *= 0.9;
-        }
-        else if (age>65) {
-            total *= 0.7;
-        }else {
-            isError=true;
-        }
-
-        switch (type){
-            case 2:
-                total *= 0.8;
-                total *= 2;
-                break;
-            case 1:
-                total = total;
-                break;
-            default:
-                isError = true;
+        tripType=input.nextInt();
+        // girilen degerlerin dogru aralikta olup olmadigina bakiyoruz
+        if ((distance>0) && (age>0) && (tripType==1 || tripType==2)){
+            // uzaklik ile tutar hesaplandi
+            total=distance*0.10;
+            // yas indirimi kismi
+            if(age<12){
+                total-=total*0.5; // tutarin yarisini cikar ve degiskene ata
+            }if (age>=12 && age<=24) {
+                total-=total*0.1;
+            }if (age>=65){
+                total-=total*0.3;
+            }
+            // yolculuk tipine gore indirim kismi
+            if(tripType==2){
+                total -= total*0.2; // gidis-donus ise indirim yapılıyor
+                total *=2; // iki yonlu oldugu icin gidis-gelis tutar hesaplandi
+            }
+            // sonuc kismi
+            System.out.println("Toplam Tutar = "+total+" TL");
+        }else{
+            // verilere istenilmeyen aralikta deger verildiyse dondurelecek hata kismi
+            System.out.println("Hatali veri tipi girdiniz!");
         }
 
-        if (!(km>=0)){
-            isError = true;
-        }
-
-        if (isError == true){
-            System.out.println("Hatalı Veri Girdiniz !");
-        }
-        else{
-            System.out.println("Toplam Tutar: " + total + " TL");
-        }
     }
 }
